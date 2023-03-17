aea2969dfa9d5095e5d0ea518f75ad892f3447d9
Akshay-rajendran <akshayaromal9@gmail.com>
2023-03-16 20:46:15 -0700

    netflix Login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import '../Login.css'
 function Login() {
   return (
-    <div>Login</div>
+    <div className='Loginblend'>
+      <div className='nav'>
+        <img className='loginlogo' src="/image/netflix.png" alt=""  />
+        <div>
+          <select name="" className='select'>
+            <option value="volvo">English</option>
+            <option value="saab">Hindi</option>
+          </select>
+          <button className='back'>sign in</button>
+        </div>
+      </div>
+      <h1>Unlimited movies, TV shows and more.</h1>
+      <div className='h2'>Watch anywhere. Cancel anytime.</div>
+      <div className='h3'>Ready to watch? Enter your email to create or restart your membership.</div>
+      <div className='gmail-button'>
+        <input type="text" className='gmail' placeholder='Email address' />
+        <button className='gohome'>Get started </button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import '../Signup.css'
+import {Link,useNavigate} from 'react-router-dom'
+import { useState,useRef } from 'react'
 
-function Signup() {
+function Signup(props) {
+
+
+    const nameRef=useRef()
+    const passRef=useRef()
+    const navigate=useNavigate()
+
+    function submit(){
+        let user={name:nameRef.current.value,password:passRef.current.value}
+        props.setuser(user)
+        navigate("/Login")
+    }
     return (
         <>
             <div className='body'>
-                <img src="../image/netflix.png" alt="Netflix" />
+                <img src="/image/netflix.png" className='netflixlogo' alt="Netflix" />
 
                 <div className='Signup'>
                     <h2>Sign In</h2>
-                    <input type="text" className='input' placeholder='email or phone number' /><br />
-                    <input type="text" className='input' placeholder='password' /><br />
-                    <button className='button'>sign in</button>
+                    <input type="text" className='input' placeholder='email or phone number' ref={nameRef} /><br />
+                    <input type="text" className='input' placeholder='password' ref={passRef} /><br />
+                    <button onClick={submit} className='button'>sign in</button>
                     <div className='checkbox'>
                         <div>
                             <input type="checkbox" />
@@ -19,7 +32,7 @@ function Signup() {
                         </div>
                         <h5>Need help?</h5>
                     </div>
-                    <h3>New to Netflix? <a href="" className='signupnow'>Sign up now.</a></h3>
+                    <h3>New to Netflix? <a href="" ><Link to="Login" className='signupnow' >Sign up now.</Link></a></h3>
                     <div className='p'>This page is protected by Google reCAPTCHA to</div>
                     <div className='p'>ensure you're not a bot. <a href="">Learn more.</a></div>
                 </div>
